39ae85b292ad2241a21af01e207ab4ddd5ec98c1
brock <niklasbrock@gmail.com>
2019-11-11 16:13:09 +0100

    added databaseadapter

--- src/main/java/dat19v2/niklasbrock/gardatours/service/DatabaseAdapter.java
+++ src/main/java/dat19v2/niklasbrock/gardatours/service/DatabaseAdapter.java
@@ -6,18 +6,17 @@ import java.sql.SQLException;
 
 public class DatabaseAdapter {
 
-    private static String url = "jdbc:mysql://localhost:3306/prototypeeop";
+    private static String url = "jdbc:mysql://localhost:3306/gardago";
     private static String driverName = "com.mysql.jdbc.Driver";
     private static String username = "root";
-    private static String password = "triala";
+    private static String password = "rootkode";
     private static Connection con;
-    private static String urlstring;
 
     public static Connection getConnection() {
         try {
             Class.forName(driverName);
             try {
-                con = DriverManager.getConnection(urlstring, username, password);
+                con = DriverManager.getConnection(url, username, password);
             } catch (SQLException ex) {
                 // log an exception. fro example:
                 System.out.println("Failed to create the database connection.");
