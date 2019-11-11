ba07778ec7bf6ba4c66f9d405628cffefb7e6e8e
mikaelpasovski <mikaelpasovski@gmail.com>
2019-11-11 15:51:31 +0100

    wadawdawd

--- src/main/java/dat19v2/niklasbrock/gardatours/service/DAO.java
+++ src/main/java/dat19v2/niklasbrock/gardatours/service/DAO.java
@@ -2,7 +2,7 @@ package dat19v2.niklasbrock.gardatours.service;
 
 public interface DAO {
 
-//    Andreas er en luder xd;
+//       Andreas er en luder xd;
 //    Og Frederik er en bitch
     //MIKAEL ER RELEVANT
     // NIKLAS ER MIN LILLE BITCH :3
