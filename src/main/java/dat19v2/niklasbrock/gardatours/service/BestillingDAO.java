88d754e0b56af624d139841001e9e1118b7eb7c5
brock <niklasbrock@gmail.com>
2019-11-11 15:59:55 +0100

    removed bullshit

--- src/main/java/dat19v2/niklasbrock/gardatours/service/BestillingDAO.java
+++ src/main/java/dat19v2/niklasbrock/gardatours/service/BestillingDAO.java
@@ -1,6 +1,13 @@
 package dat19v2.niklasbrock.gardatours.service;
 
-public class BestillingDAO {
+public class BestillingDAO implements IDAO{
+
+
+    public void insert(){};
+    public void delete(){};
+    public String select(){
+        return null;
+    };
 
 
 }
--- src/main/java/dat19v2/niklasbrock/gardatours/service/IDAO.java
+++ src/main/java/dat19v2/niklasbrock/gardatours/service/IDAO.java
@@ -1,10 +1,10 @@
 package dat19v2.niklasbrock.gardatours.service;
 
-public interface DAO {
+public interface IDAO {
 
     public void insert();
     public void delete();
-    public void select();
+    public String select();
 
 
 }
