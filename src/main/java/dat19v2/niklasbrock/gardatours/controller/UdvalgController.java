bf31b9dcd00e63339eec337be6dd020b572767c3
mikaelpasovski <mikaelpasovski@gmail.com>
2019-11-14 11:33:38 +0100

    html og css, det' bar' fint

--- src/main/java/dat19v2/niklasbrock/gardatours/controller/UdvalgController.java
+++ src/main/java/dat19v2/niklasbrock/gardatours/controller/UdvalgController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UdvalgController {
-    @GetMapping("/index")
+    @GetMapping("/")
     public String home() {
-        return "index";
+        return "udvalg";
     }
 }
