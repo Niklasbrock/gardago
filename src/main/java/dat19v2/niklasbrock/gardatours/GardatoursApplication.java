4bd4a8e5a480d8c0b2c5c2261080061c03283e9e
nullxDEADBEEF <andxdgu@gmail.com>
2019-11-14 13:55:39 +0100

    to be able to pull

--- src/main/java/dat19v2/niklasbrock/gardatours/GardatoursApplication.java
+++ src/main/java/dat19v2/niklasbrock/gardatours/GardatoursApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GardatoursApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(GardatoursApplication.class, args);
     }
--- src/main/java/dat19v2/niklasbrock/gardatours/service/RuterDAO.java
+++ src/main/java/dat19v2/niklasbrock/gardatours/service/RuterDAO.java
@@ -1,8 +1,37 @@
 package dat19v2.niklasbrock.gardatours.service;
 
-public class RuterDAO implements IDAO{
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
-    public void insert(){};
+public class RuterDAO implements IDAO {
+
+    // NOTE: DOESNT MATTER....
+    public void insert() {
+        String sql = "INSERT INTO Ruter" + " (Pris, Ruter, Beskrivelse) " +
+                "VALUES (?, ?, ?)";
+        Connection connection = null;
+
+        try {
+            connection = DatabaseAdapter.getInstance().getConnection();
+            PreparedStatement ps = connection.prepareStatement( sql );
+            ps.setInt( 1, 666 );
+            ps.setString( 2, "Garda til helvede xD" );
+            ps.executeUpdate();
+            ps.close();
+        } catch( Exception ex ) {
+            System.err.println( "ERROR: " + ex );
+        } finally {
+            if ( connection != null ) {
+                try {
+                    connection.close();
+                } catch ( SQLException ex ) {
+                    System.err.println( "ERROR " + ex );
+                 }
+            }
+        }
+
+    };
     public void delete(){};
     public String select(){
         return null;
