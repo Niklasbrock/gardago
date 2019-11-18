aca389d7b8b5b837cb5072aa8c88f1186843a7df
brock <niklasbrock@gmail.com>
2019-11-18 15:29:29 +0100

    faaet js til at virke. Nu mangler jeg at faa thymeleaf prisen

--- src/main/resources/static/javascript/script.js
+++ src/main/resources/static/javascript/script.js
@@ -1,6 +1,17 @@
-function updateAntal() {
-    var total = document.getElementById("antal") * document.getElementById("totalPris");
-    pris = document.createElement("pris");
-    pris.innerHTML = total;
-    document.body.appendChild(pris);
+document.getElementById("pris").innerHTML = updateAntal();
+
+function addTotalElement() {
+    var totalpris = document.createElement("totalpris");
+    var node = document.createTextNode(updateAntal());
+    totalpris.appendChild(node);
+    document.tbody.appendChild(totalpris);
 }
+
+function updateAntal() {
+    var pris = parseInt(document.getElementById("pris"));
+    var antal = document.getElementById("antal");
+    pris *= antal;
+    // document.getElementById("pris").setAttribute("text", pris);
+    return pris;
+}
+
