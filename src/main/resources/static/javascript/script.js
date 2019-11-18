5d5dcfa050656647e59c7a5cb8b16bd89031ee72
Niklas Lund Brock <niklasbrock@gmail.com>
2019-11-18 23:02:59 +0100

    Fikset at priserne / antal virker i checkout med de rigtige priser fra databasen.

--- src/main/resources/static/javascript/script.js
+++ src/main/resources/static/javascript/script.js
@@ -1,31 +1,21 @@
-document.getElementById("pris").innerHTML = updateAntal();
+// document.getElementById("pris").innerHTML = updateAntal();
+// function updateAntal() {
+//     //TODO Get Thymeleaf price to work here.55
+//     // var pris = document.getElementById("pris").value;
+//     var pris = 666;
+//     var antal = document.getElementById("antal").value;
+//     pris *= antal;
+//     document.getElementById("pris").setAttribute("value", pris);
+//     return pris;
+// }
+
+
+document.getElementById("pris").innerHTML = document.getElementById("pris").value;
+
 function updateAntal() {
-    //TODO Get Thymeleaf price to work here.55
-    // var pris = document.getElementById("pris").value;
-    var pris = 666;
+    var pris = document.getElementById("pris").value;
     var antal = document.getElementById("antal").value;
     pris *= antal;
     document.getElementById("pris").setAttribute("value", pris);
-    return pris;
 }
 
-
-
-
-// document.getElementById("pris").innerHTML = updateAntal();
-//
-// function addTotalElement() {
-//     var totalpris = document.createElement("totalpris");
-//     var node = document.createTextNode(updateAntal());
-//     totalpris.appendChild(node);
-//     document.tbody.appendChild(totalpris);
-// }
-//
-// function updateAntal() {
-//     var pris = parseInt(document.getElementById("pris"));
-//     var antal = document.getElementById("antal");
-//     pris *= antal;
-//     // document.getElementById("pris").setAttribute("text", pris);
-//     return pris;
-// }
-//
