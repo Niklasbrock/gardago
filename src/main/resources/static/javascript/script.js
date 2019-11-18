12c13b0e625d38d98bbaecb2010e9137202feb99
Niklas Lund Brock <niklasbrock@gmail.com>
2019-11-18 22:50:30 +0100

    Faaet linked Javascript filen ordentligt i checkout.html

--- src/main/resources/static/javascript/script.js
+++ src/main/resources/static/javascript/script.js
@@ -1,17 +1,31 @@
 document.getElementById("pris").innerHTML = updateAntal();
-
-function addTotalElement() {
-    var totalpris = document.createElement("totalpris");
-    var node = document.createTextNode(updateAntal());
-    totalpris.appendChild(node);
-    document.tbody.appendChild(totalpris);
-}
-
 function updateAntal() {
-    var pris = parseInt(document.getElementById("pris"));
-    var antal = document.getElementById("antal");
+    //TODO Get Thymeleaf price to work here.55
+    // var pris = document.getElementById("pris").value;
+    var pris = 666;
+    var antal = document.getElementById("antal").value;
     pris *= antal;
-    // document.getElementById("pris").setAttribute("text", pris);
+    document.getElementById("pris").setAttribute("value", pris);
     return pris;
 }
 
+
+
+
+// document.getElementById("pris").innerHTML = updateAntal();
+//
+// function addTotalElement() {
+//     var totalpris = document.createElement("totalpris");
+//     var node = document.createTextNode(updateAntal());
+//     totalpris.appendChild(node);
+//     document.tbody.appendChild(totalpris);
+// }
+//
+// function updateAntal() {
+//     var pris = parseInt(document.getElementById("pris"));
+//     var antal = document.getElementById("antal");
+//     pris *= antal;
+//     // document.getElementById("pris").setAttribute("text", pris);
+//     return pris;
+// }
+//
